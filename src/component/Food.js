be49624d6d845b13d9b3b53aed6d0dee523a73e9
kokiku1700 <kokiku1700@naver.com>
2025-01-14 18:04:59 +0900

    2025-01-14

--- src/component/Food.js
+++ src/component/Food.js
@@ -3,47 +3,44 @@ import WorldCup from "./WorldCup";
 import * as foodImg from "./foodImgUpload";
 
 const Food = () => {
-    const [imgArr, setImgArr] = useState([
-        {
-          id: 0,
-          name: "쌀국수",
-          src: foodImg.food0,
-        },
-        {
-          id: 1,
-          name: "라면",
-          src: foodImg.food1,
-        },
-        {
-          id: 2,
-          name: "삼겹살",
-          src: foodImg.food2,
-        },
-        {
-          id: 3,
-          name: "부대찌개",
-          src: foodImg.food3,
-        },
-        {
-          id: 4,
-          name: "피자",
-          src: foodImg.food4,
-        },
-        {
-          id: 5,
-          name: "돈까스",
-          src: foodImg.food5,
-        },
-      ]);
-      // imgArr.sort(() => Math.random() - 0.5)
-      
-      const [ideal, setIdeal] = useState([]);
+  const foods = [
+    {
+      id: 0,
+      name: "쌀국수",
+      src: foodImg.food0,
+    },
+    {
+      id: 1,
+      name: "라면",
+      src: foodImg.food1,
+    },
+    {
+      id: 2,
+      name: "삼겹살",
+      src: foodImg.food2,
+    },
+    {
+      id: 3,
+      name: "부대찌개",
+      src: foodImg.food3,
+    },
+    {
+      id: 4,
+      name: "피자",
+      src: foodImg.food4,
+    },
+    {
+      id: 5,
+      name: "돈까스",
+      src: foodImg.food5,
+    },
+  ]
 
-    return (
-        <div className="Food">
-            <WorldCup imgArr={imgArr} ideal={ideal} setideal={setIdeal} />
-        </div>
-    );
+  return (
+    <div className="Food">
+      <WorldCup arr={foods} />
+    </div>
+  );
 };
 
 export default Food;
--- src/component/WorldCup.js
+++ src/component/WorldCup.js
@@ -1,19 +1,51 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const WorldCup = ({ imgArr, ideal, setIdeal }) => {
+const WorldCup = ({ arr }) => {
+    const [list, setList] = useState([]);
+    const [viewImg, setViewImg] = useState([]);
+    const [winner, setWinner] = useState([]);
+    const [count, setCount] = useState(2);
+    
+    useEffect(() => {
+        arr.sort(() => Math.random() - 0.5);    
+        setList(arr);
+        setViewImg([arr[0], arr[1]]);
+    }, [])
+
+    const onClickImg = ( i ) => {
+        setWinner([...winner, arr[i === 0 ? count - 2 : count - 1]]);
+        setViewImg([arr[count], arr[count + 1]]);
+        setCount(count + 2);
+        console.log(winner)
+    }
 
     return (
         <div>
-            {imgArr.map(e => (
-                <StyledImg src={e.src} alt="" key={e.id} />
-            ))}
+            <StyledPH1>음식 이상형 월드컵 ({count} / {list.length})</StyledPH1>
+            <StyledDiv>
+                {viewImg.map((e, idx) => (
+                    <StyledImg src={e.src} alt="" key={e.id} onClick={() => onClickImg(idx)} />
+                ))}
+            </StyledDiv>
         </div>
+        
     );
 };
+
+const StyledPH1 = styled.h1`
+    text-align: center;
+`
+
+const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
 
 const StyledImg = styled.img`
     width: 40vw;
     height: 30vw;
+    margin: 0% 1%
 `
 
 export default WorldCup;
